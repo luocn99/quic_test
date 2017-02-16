ecb5f4f0a9cefd0f54469845364dfb3dcf7d22f6
luocn99 <luocn99@163.com>
2017-02-16 19:28:58 +0800

    add rejection.png

--- digraph.py
+++ digraph.py
@@ -9,20 +9,30 @@ import re
 import os
 
 #grep -o "\:\:.*(" gdb.record|awk -F '(' '{print $1}' >func_frame
-
-def parse_log(log):
-    with open(log, "r") as f:
+dot_file = "auto.dot"
+func_file = "func_frame"
+def make_fun():
+    cmd = "grep -o '\:\:.*(' gdb.record|awk -F '(' '{print $1}' >func_frame"
+    os.system(cmd)
+def make_pic():
+    os.system("dot -Tpng %s -o %s.png" % (dot_file, sys.argv[1]))
+def parse_frame():
+    of = open(dot_file, "w") 
+    of.write("digraph G {\n")
+    with open(func_file, "r") as f:
         lines = f.readlines()
         num = len(lines)
         i = num - 1
         print "digraph G {"
         while i > 0:
             print "\"%s\"->\"%s\"" % (lines[i].strip("\n"), lines[i-1].strip("\n"))
+            of.write("\"%s\"->\"%s\" \n" % (lines[i].strip("\n"), lines[i-1].strip("\n")))
             i -= 1
         print "}"
+        of.write("}")
+    of.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 :
-        print("wrong arg num, usage: log_file")
-        exit(1)
-    parse_log(sys.argv[1])
+    make_fun()
+    parse_frame()
+    make_pic()
